de5253e26fce7833f8439ed9f474ec69909c9ed3
Elisa Demanez <demanez.elisa@gmail.com>
2018-06-15 15:07:01 +0200

    not finished bot functionning

---
+++ src/components/utilitaries/NavigationBottom.vue
@@ -0,0 +1,59 @@
+
+<template>
+
+
+ <v-card height="150px" flat>
+
+
+    <v-bottom-nav :value="true" :active.sync="e1" absolute color="transparent"  id='navbar' class="hidden-sm-and-up" >
+      <v-btn flat color="teal" value="Home" :to="{name:'Home'}">
+        <span>Home</span>
+        <v-icon>home</v-icon>
+      </v-btn>
+      <v-btn flat color="teal" value="Ajouter" :to="{name:'Ajouter'}">
+        <span>Add a plant</span>
+        <v-icon>add</v-icon>
+      </v-btn>
+  
+    </v-bottom-nav>
+
+ 
+  </v-card>
+
+</template>
+
+<script>
+/* eslint-disable */
+
+export default {
+  name: "NavigationBottom",
+  
+  components: {
+        
+    },
+
+  data() {
+    return {
+    
+    };
+  },
+  mounted() {
+  },
+  methods: {
+   
+  },
+  computed: {
+
+  }
+};
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped >
+#navbar {
+
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
+</style>
---
+++ src/components/utilitaries/NavigationTop.vue
@@ -0,0 +1,55 @@
+
+
+<template>
+
+
+ 
+   <v-toolbar class="hidden-sm-and-down navbar">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>Title</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items >
+        <v-btn flat color="teal" value="Home" :to="{name:'Home'}">
+        <span>Home</span>
+      </v-btn>
+      <v-btn flat color="teal" value="Ajouter" :to="{name:'Ajouter'}">
+        <span>Add a plant</span>
+      </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+
+</template>
+
+<script>
+/* eslint-disable */
+
+export default {
+  name: "NavigationTop",
+  
+  components: {
+        
+    },
+
+  data() {
+    return {
+    
+    };
+  },
+  mounted() {
+  },
+  methods: {
+   
+  },
+  computed: {
+
+  }
+};
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped >
+.navbar {
+
+  width: 100vw;
+}
+</style>
