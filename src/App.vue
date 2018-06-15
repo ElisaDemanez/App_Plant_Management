0c23c96159844ca970db521b1899e4b21a9ade64
Elisa Demanez <demanez.elisa@gmail.com>
2018-06-15 15:06:02 +0200

    + vuetify flex + both nav bars

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,40 @@
 <template>
-  <div id="app">
-    <router-view/>
-    <div id=navbar>
-      <router-link to='/'>Accueil </router-link>
-      <span>|</span>
-       <router-link to='/ajouter'>Ajouter une plante : </router-link>
-    </div>
-  </div>
+<v-app id="app">
+ 
+<NavigationTop />
+  <v-container d-flex  color="purple">
+      <v-layout row wrap>
+        <v-flex xs12 sm8 md6 lg6 xl4 offset-sm2 offset-md3 offset-lg3 offset-sm5 >
+          <router-view/>
+        </v-flex>
+      </v-layout>
+  </v-container>
+
+<NavigationBottom />
+</v-app>
 </template>
 
 <script>
+import NavigationBottom from "@/components/utilitaries/NavigationBottom.vue";
+import NavigationTop from "@/components/utilitaries/NavigationTop.vue";
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: {
+    NavigationBottom,
+    NavigationTop
+  }
+};
 </script>
 
 <style>
 #app {
-  width: 400px;
+  /* width: 400px; */
   margin: auto;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
