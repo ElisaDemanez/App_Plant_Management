539a4b2ff18af452b8ddb391a67e8643907ad154
Elisa Demanez <demanez.elisa@gmail.com>
2018-06-17 22:32:35 +0200

    + loading

--- src/components/Liste.vue
+++ src/components/Liste.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="liste">
-      <ul class="list">
+      <!-- <ul class="list">
      
        <v-list two-line>
           <template v-for="(plant, index) in plantList">
@@ -15,7 +15,27 @@
             </v-list-tile>
           </template>
         </v-list>
-    </ul>
+    </ul> -->
+      <v-progress-circular v-if="loading" indeterminate ></v-progress-circular>
+        <v-card v-else>
+     
+       <v-list two-line>
+          <template v-for="(plant, index) in plantList">
+           <v-list-tile  :key="plant.id" avatar ripple @click="{}">
+              <v-list-tile-content :key="index">
+                <v-list-tile-title v-html="'#'+plant.id +': '+ plant.name"></v-list-tile-title>
+                <v-list-tile-sub-title v-html="plant.seller"></v-list-tile-sub-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+              <v-icon   @click="updatePlant(plant.id)"   color="teal">update</v-icon>
+              <v-icon  @click="deletePlant(plant.id)" color="deep-orange darken-2">delete</v-icon>
+              
+            </v-list-tile-action>
+           </v-list-tile>
+            <v-divider :key="index" inset ></v-divider>
+          </template>
+        </v-list>
+  </v-card>
   </div>
 </template>
 
@@ -26,6 +46,7 @@ export default {
   name: "Liste",
   data() {
     return {
+      loading: true,
       plantList: []
     };
   },
@@ -34,8 +55,9 @@ export default {
   },
   created() {
     var plants = [];
-    // db doest seems to work in mounted
+    var here = this;
     connection.ref("plants").once("value", function(snapshot) {
+      console.log("1");
       snapshot.forEach(function(childSnapshot) {
         var id = childSnapshot.key;
         var name = childSnapshot.val().name;
@@ -45,27 +67,37 @@ export default {
         plant["name"] = name;
         plant["seller"] = seller;
         plants.push(plant);
+        // replace the seller id
       });
-    });
-    
-    // replace the seller id
-    plants.forEach(plant => {
-  
-      for (const seller in this.db[2]) {
-        if (this.db[2].hasOwnProperty(seller)) {
-          const element = this.db[2][seller];
+      plants.forEach(plant => {
+        console.log("2");
 
-          if (plant.seller == seller) {
-            plant['seller'] = element.name;
+        for (const seller in here.db[2]) {
+          if (here.db[2].hasOwnProperty(seller)) {
+            const element = here.db[2][seller];
+
+            if (plant.seller == seller) {
+              plant["seller"] = element.name;
+              // console.log(plant["seller"]);
+            }
           }
         }
-      }
+      });
+      here.loading = false; 
     });
+
     this.plantList = plants;
+  },
+  methods: {
+    deletePlant: function(id) {
+      console.log(id);
+    },
+    updatePlant: function(id) {
+      console.log(id);
+    }
   }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
 </style>
