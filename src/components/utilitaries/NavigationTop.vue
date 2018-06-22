8e7f2fdbdadd1305c7e3e5ea803a77e5075305ac
Elisa Demanez <demanez.elisa@gmail.com>
2018-06-22 12:17:30 +0200

    + miscellanous dropdown

--- src/components/utilitaries/NavigationTop.vue
+++ src/components/utilitaries/NavigationTop.vue
@@ -18,14 +18,20 @@
        <v-menu offset-y>
       <v-btn flat slot="activator" color="teal" >Miscellaneous</v-btn>
       <v-list >
-        <v-list-tile   v-for="(item, index) in miscellaneous" :key="index" :to="{name: item.route}">
+        <v-list-tile   v-for="(item, index) in miscellaneous" :key="index" :to="{name: 'AjouterMisc', params : { object : item.param} }">
           <v-list-tile-title class="teal--text" >{{ item.title }}</v-list-tile-title>
         </v-list-tile>
       </v-list>
     </v-menu>
     </v-toolbar-items>
   </v-toolbar>
-
+ <!-- this.$router.push({
+        name: "Ajouter",
+        params: {
+          id: id,
+          plantToUpdate: this.plantsRef[id]
+        }
+      }); -->
 </template>
 
 <script>
@@ -39,8 +45,8 @@ export default {
   data() {
     return {
       miscellaneous: [
-        { title: "Ajouter un vendeur", route: "addSeller" },
-        { title: "Ajouter une espèce", route: "addSpecie" }
+        { title: "Ajouter un vendeur", route: "addSeller", param: "sellers" },
+        { title: "Ajouter une espèce", route: "addSpecie", param: "species" }
       ]
     };
   },
