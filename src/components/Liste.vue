aceac534265407f7139606419940a3dbe962fbcd
Elisa Demanez <demanez.elisa@gmail.com>
2018-06-26 17:32:24 +0200

    ugly grid, before rework

--- src/components/Liste.vue
+++ src/components/Liste.vue
@@ -1,51 +1,45 @@
 <template>
-  <div class="liste">
+    <div class="liste">
+        <v-container fluid grid-list-sm>
+            <v-layout row wrap>
 
-      <v-list two-line>
-        <template v-for="(plant, index) in plants">
+                <template v-for="(plant, index) in plants">
+                    <v-flex xs12 md6 :key="plant.id">
+                        <v-card color="grey lighten-4">
+                            <v-list-tile :key="plant.id" avatar ripple @click="active =plant" @click.stop="dialog = true">
+                                <v-list-tile-content :key="index">
+                                    <v-list-tile-title>
+                                        #{{plant.id}}:
+                                        <strong v-if="plant.speciesName" class="hidden-sm-and-down" v-html="plant.speciesName">
+                                        </strong>
+                                        <strong v-if="plant.speciesName" class="hidden-sm-and-up" v-html="plant.speciesName.slice(0, 4) + '.'">
+                                        </strong>
+                                        {{plant.subspName}}
+                                    </v-list-tile-title>
+                                    <v-list-tile-sub-title>
+                                        <v-layout row justify-space-between>
+                                            <v-flex xs10>
+                                                <span class="text-xs-right">{{plant.exposure}}</span>
+                                            </v-flex>
+                                            <v-flex xs2>
+                                                <span class="text-xs-left">{{plant.temperature}}°</span>
+                                            </v-flex>
+                                        </v-layout>
+                                    </v-list-tile-sub-title>
+                                </v-list-tile-content>
+                            </v-list-tile>
+                        </v-card>
+                    </v-flex>
+                </template>
+                
+            </v-layout>
+        </v-container>
 
-          <v-list-tile :key="plant.id" avatar ripple  @click="active =plant" @click.stop="dialog = true">
-      
-            <v-list-tile-content :key="index">
-              <v-list-tile-title>
-                #{{plant.id}}:
-                <strong v-if="plant.speciesName" class="hidden-sm-and-down" v-html="plant.speciesName">
-                </strong>
-                <strong v-if="plant.speciesName" class="hidden-sm-and-up" v-html="plant.speciesName.slice(0, 4) + '.'">
-                </strong>
-                {{plant.subspName}}
-              </v-list-tile-title>
-              <v-list-tile-sub-title>
-                <v-layout row justify-space-between>
-                  <v-flex xs10>
-                    <span class="text-xs-right">{{plant.exposure}}</span>
-                  </v-flex>
-                  <v-flex xs2>
-                    <span class="text-xs-left">{{plant.temperature}}°</span>
-                  </v-flex>
-                </v-layout>
-              </v-list-tile-sub-title>
+        <plantDetailsDialog v-if="dialog" :dialog="dialog" :plant="active" @close="dialog=false" />
 
-            </v-list-tile-content>
-            <!-- <v-list-tile-action>
+    </div>
 
-              <v-icon @click="updatePlant(plant)" color="teal">update</v-icon>
-
-              <v-icon @click="deletePlant(plant.id)" color="deep-orange darken-2">delete</v-icon>
-
-            </v-list-tile-action> -->
-          </v-list-tile>
-          <v-divider :key="index" inset></v-divider>
-        </template>
-
-      </v-list>
-
-     <plantDetailsDialog v-if="dialog" :dialog="dialog"
-      :plant="active" @close="dialog=false"/>
-
-  </div>
-
-</template>
+</template> 
 
 <script>
 /* eslint-disable */
@@ -53,26 +47,25 @@
 import { connection } from "@/components/firebase.js";
 import plantDetailsDialog from "@/components/utilitaries/plantDetailsDialog";
 
-
 export default {
   name: "Liste",
   data() {
     return {
-       dialog: false,
-      active :'',
+      dialog: false,
+      active: ""
     };
   },
-  components : {
-    plantDetailsDialog :plantDetailsDialog
+  components: {
+    plantDetailsDialog: plantDetailsDialog
   },
   props: {
-    plants: Array,
+    plants: Array
   },
   firebase: {
     plantsRef: {
       source: connection.ref("plants"),
       asObject: true
-    },
+    }
   },
   computed: {},
   methods: {
