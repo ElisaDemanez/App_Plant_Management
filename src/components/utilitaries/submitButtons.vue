dca9e6e74ca6eb9e582e0bf85995fd57e6143c76
Elisa Demanez <demanez.elisa@gmail.com>
2018-06-26 14:10:39 +0200

    submit + cancel button

---
+++ src/components/utilitaries/submitButtons.vue
@@ -0,0 +1,37 @@
+ 
+
+      <template>
+  <div class='submit'>
+   <v-container>
+        <v-btn @click="$emit('form-validation')" color='primary' type='submit' value='Submit' >Submit </v-btn>
+        <v-spacer></v-spacer>
+      </v-container>
+
+      <v-container>
+        <router-link :to="{name: 'Home'}" class="right">
+          <v-btn flat small>Cancel</v-btn>
+        </router-link>
+      </v-container>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+
+
+export default {
+  name: "submitButtons",
+  data() {
+    return {
+      
+    };
+  },
+  
+  methods: {
+    
+  }
+};
+</script>
+
+<style scoped >
+</style>
