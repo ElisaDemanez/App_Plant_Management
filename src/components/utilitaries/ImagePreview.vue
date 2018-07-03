19f9edec6f5c411328616cc43ddee11b644f8c1e
Elisa Demanez <demanez.elisa@gmail.com>
2018-07-03 20:51:28 +0200

    1st commit

---
+++ src/components/utilitaries/ImagePreview.vue
@@ -0,0 +1,39 @@
+ 
+
+<template>
+  <v-flex xs12 md6 >
+    <img  :src="src" alt="" style="width:100%">
+  </v-flex>
+</template>
+
+<script>
+/* eslint-disable */
+// credits : https://jsfiddle.net/o1037uks/
+
+export default {
+  name: "ImagePreview",
+  data() {
+    return {
+      src: ""
+    };
+  },
+  props: ["file"],
+
+  methods: {
+    loadPicture(file) {
+      let self = this;
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        self.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  },
+  mounted() {
+    this.loadPicture(this.file);
+  }
+};
+</script>
+
+<style scoped >
+</style>
