f9472c647c32fcab6f409537ebac473b6cd85a7e
Elisa Demanez <demanez.elisa@gmail.com>
2018-07-03 20:50:11 +0200

    + gallery for multiple images

--- src/components/utilitaries/plantDetailsDialog.vue
+++ src/components/utilitaries/plantDetailsDialog.vue
@@ -2,7 +2,7 @@
     <div class='details'>
         <v-dialog lazy v-model="dialog" max-width="500px">
             <v-card class="text-xs-left">
-                <v-card-media v-if="plant.imgURL" :src="plant.imgURL" height="200px">
+                <v-card-media v-if="plant.images" :src="plant.images[Object.keys(plant.images)[0]].url" height="200px">
                 </v-card-media>
                 <v-card-title primary-title>
                     <h3 class="headline mb-0">
@@ -11,17 +11,17 @@
                         <span class="weight-800">{{plant.speciesName}} </span>
                         <span class="grey--text weight-100">{{plant.subspName}} </span>
                     </h3>
-
                 </v-card-title>
-
 
                 <v-card-text>
                     <h4 class="primary--text">Infos</h4>
                     <p class="grey--text text--darken-3 subheading mb-1">Seller : {{plant.sellerName}}</p>
 
                     <p class="grey--text text--darken-3 subheading">Exposure : {{plant.exposure}}</p>
-                    <h4 class="primary--text mb-1" v:if="plant.description">Détails</h4>
+                    <h4 class="primary--text mb-1" v:if="plant.description">Details</h4>
                     <p> {{plant.description}}</p>
+                         <h4 class="primary--text mb-1" v:if="plant.images">Gallery</h4>
+                         <Gallery :images="plant.images"/>
                 </v-card-text>
 
                 <v-card-actions>
@@ -35,18 +35,16 @@
                     </v-btn>
 
                 </v-card-actions>
-
             </v-card>
-
         </v-dialog>
-
     </div>
 </template>
 
 <script>
 /* eslint-disable */
 
-import { connection } from "@/components/firebase.js";
+import { connection, app } from "@/components/firebase.js";
+import Gallery from "@/components/utilitaries/Gallery.vue";
 
 export default {
   name: "plantDetailsDialog",
@@ -58,6 +56,7 @@ export default {
     dialog: Boolean,
     plant: Object
   },
+  components: { Gallery },
   firebase: {
     db: {
       source: connection.ref(),
@@ -77,8 +76,6 @@ export default {
   },
   methods: {
     deletePlant: function(id) {
-      console.log("aa");
-
       // // Known bug : unexplained : On computer, with mobiel display : confirm is not fired and always fake
       var oui = confirm(
         "Its like, gonna be gone, like forever. Like you sure its really dead ? You could like pop it in a lil bit of water or like idk "
@@ -86,7 +83,7 @@ export default {
       if (oui) {
         this.$firebaseRefs.plantsRef.child(id).remove();
         this.dialog = false;
-        $emit("close");
+        this.$emit("close");
       }
     },
     updatePlant: function(plant) {
